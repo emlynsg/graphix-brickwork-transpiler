d6fe6fa34dadeaba3d4d32fb4d674acc1da7e1b2
Emlyn Graham <egraham@PTB-02009984.paris.inria.fr>
2025-10-30 17:55:35 +0100

    adding new connections to JCZ

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,3 @@
 
 Copyright (C) 2025, QAT team (ENS-PSL, Inria, CNRS).
 """
-
-from graphix_brickwork_transpiler.brickwork import transpile_brickwork
-
-__all__ = ["transpile_brickwork"]
--- tests/test_brickwork_transpiler.py
+++ tests/test_brickwork_transpiler.py
@@ -6,7 +6,8 @@ import numpy as np
 import pytest
 from graphix.parameter import Placeholder
 from graphix.transpiler import Circuit
-from graphix_brickwork_transpiler.brickwork import _nqubits_from_layers, transpile_to_layers  # noqa: PLC2701
+
+from graphix_brickwork_transpiler import nqubits_from_layers, transpile_brickwork, transpile_to_layers  # noqa: PLC2701
 
 if TYPE_CHECKING:
     from numpy.random import Generator
@@ -16,7 +17,7 @@ class TestBrickworkTranspilerUnitGates:
     def test_cnot(self, fx_rng: Generator) -> None:
         circuit = Circuit(2)
         circuit.cnot(0, 1)
-        pattern = circuit.transpile(method="brickwork").pattern
+        pattern = transpile_brickwork(circuit).pattern
         state = circuit.simulate_statevector().statevec
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
@@ -25,7 +26,7 @@ class TestBrickworkTranspilerUnitGates:
         theta = fx_rng.uniform() * 2 * np.pi
         circuit = Circuit(2)
         circuit.rx(0, theta)
-        pattern = circuit.transpile(method="brickwork").pattern
+        pattern = transpile_brickwork(circuit).pattern
         state = circuit.simulate_statevector().statevec
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
@@ -34,7 +35,7 @@ class TestBrickworkTranspilerUnitGates:
         theta = fx_rng.uniform() * 2 * np.pi
         circuit = Circuit(2)
         circuit.rz(0, theta)
-        pattern = circuit.transpile(method="brickwork").pattern
+        pattern = transpile_brickwork(circuit).pattern
         state = circuit.simulate_statevector().statevec
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
@@ -42,7 +43,7 @@ class TestBrickworkTranspilerUnitGates:
     def test_i(self, fx_rng: Generator) -> None:
         circuit = Circuit(2)
         circuit.i(0)
-        pattern = circuit.transpile(method="brickwork").pattern
+        pattern = transpile_brickwork(circuit).pattern
         state = circuit.simulate_statevector().statevec
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
@@ -53,7 +54,7 @@ class TestBrickworkTranspilerUnitGates:
             for j in [0, 1, 2, 3, 4]:
                 circuit.rx(i, j * np.pi / 4)
                 circuit.rz(i, j * np.pi / 4)
-        pattern = circuit.transpile(method="brickwork").pattern
+        pattern = transpile_brickwork(circuit).pattern
         state = circuit.simulate_statevector().statevec
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
@@ -64,7 +65,7 @@ class TestBrickworkTranspilerUnitGates:
         circuit.rx(0, np.pi / 4)
         circuit.rz(1, np.pi / 2)
         circuit.cnot(2, 1)
-        pattern = circuit.transpile(method="brickwork").pattern
+        pattern = transpile_brickwork(circuit).pattern
         state = circuit.simulate_statevector().statevec
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
@@ -77,7 +78,7 @@ class TestBrickworkTranspilerUnitGates:
         circuit.cnot(2, 1)
         pattern_default = circuit.transpile().pattern
         state_mbqc_default = pattern_default.simulate_pattern(rng=fx_rng)
-        pattern_brickwork = circuit.transpile(method="brickwork").pattern
+        pattern_brickwork = transpile_brickwork(circuit).pattern
         state_mbqc_brickwork = pattern_brickwork.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc_default.flatten().conjugate(), state_mbqc_brickwork.flatten())) == pytest.approx(1)
 
@@ -86,7 +87,7 @@ class TestBrickworkTranspilerUnitGates:
         for j in [0, 1, 2, 3, 4]:
             circuit.rx(0, j * np.pi / 4)
             circuit.i(0)
-        pattern = circuit.transpile(method="brickwork").pattern
+        pattern = transpile_brickwork(circuit).pattern
         state = circuit.simulate_statevector().statevec
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
@@ -97,7 +98,7 @@ class TestBrickworkTranspilerUnitGates:
         circuit.rx(0, np.pi / 4)
         circuit.rz(1, np.pi / 2)
         circuit.cnot(1, 2)
-        pattern = circuit.transpile(method="brickwork", order="deviant").pattern
+        pattern = transpile_brickwork(circuit, order="deviant").pattern
         state = circuit.simulate_statevector().statevec
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
@@ -108,7 +109,7 @@ class TestBrickworkTranspilerUnitGates:
         circuit.rx(0, np.pi / 4)
         circuit.rz(1, np.pi / 2)
         circuit.cnot(1, 2)
-        pattern = circuit.transpile(method="brickwork", order="deviant-right").pattern
+        pattern = transpile_brickwork(circuit, order="deviant-right").pattern
         state = circuit.simulate_statevector().statevec
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
@@ -119,7 +120,7 @@ class TestBrickworkTranspilerUnitGates:
         for i in range(n - 1):
             circuit.rx(i, 0.0)
             circuit.rz(i, 0.0)
-        pattern = circuit.transpile(method="brickwork").pattern
+        pattern = transpile_brickwork(circuit).pattern
         state = circuit.simulate_statevector().statevec
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
@@ -128,17 +129,17 @@ class TestBrickworkTranspilerUnitGates:
         circuit = Circuit(3)
         circuit.cnot(0, 2)
         with pytest.raises(ValueError):
-            circuit.transpile(method="brickwork")
+            transpile_brickwork(circuit).pattern
 
     def test_other_gate(self) -> None:
         circuit = Circuit(1)
         circuit.h(0)
         with pytest.raises(ValueError):
-            circuit.transpile(method="brickwork")
+            transpile_brickwork(circuit).pattern
 
     def test_nqubits_from_layers_0(self) -> None:
         with pytest.raises(ValueError):
-            _nqubits_from_layers([])
+            nqubits_from_layers([])
 
     def test_nqubits_from_layers_n(self) -> None:
         for i in range(2, 4):
@@ -146,7 +147,7 @@ class TestBrickworkTranspilerUnitGates:
             for j in range(i):
                 circuit.rx(j, np.pi / 4)
             layers = transpile_to_layers(circuit)
-            assert _nqubits_from_layers(layers) == i
+            assert nqubits_from_layers(layers) == i
 
     def test_parametrized_circuit(self, fx_rng: Generator) -> None:
         circuit = Circuit(4)
@@ -155,7 +156,7 @@ class TestBrickworkTranspilerUnitGates:
         for j in [0, 1, 2, 3, 4]:
             circuit.rx(0, j * alpha)
             circuit.i(0)
-        pattern = circuit.transpile(method="brickwork").pattern
+        pattern = transpile_brickwork(circuit).pattern
         assert pattern.is_parameterized()
         pattern0 = pattern.subs(alpha, np.pi / 4)
         circuit0 = circuit.subs(alpha, np.pi / 4)
