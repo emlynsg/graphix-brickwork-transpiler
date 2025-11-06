c79f141cff60fb9142cf44f65678d96e7075446d
Emlyn Graham <egraham@PTB-02009984.paris.inria.fr>
2025-11-06 14:11:45 +0100

    renamed and fixing typing

--- tests/test_brickwork_transpiler.py
+++ tests/test_brickwork_transpiler.py
@@ -6,15 +6,18 @@ import numpy as np
 import pytest
 from graphix.parameter import Placeholder
 from graphix.transpiler import Circuit
+from numpy.random import Generator
 
-from graphix_brickwork_transpiler import nqubits_from_layers, transpile_brickwork, transpile_to_layers  # noqa: PLC2701
+from graphix_brickwork_transpiler import nqubits_from_layers, transpile_brickwork, transpile_to_layers
 
-if TYPE_CHECKING:
-    from numpy.random import Generator
-
+# TODO@emlynsg: Add tests for all other gates.
 
 class TestBrickworkTranspilerUnitGates:
-    def test_cnot(self, fx_rng: Generator) -> None:
+    """Test the transpiler on circuits with single gates."""
+
+    @staticmethod
+    def test_cnot(fx_rng: Generator) -> None:
+        """Test CNOT transpilation."""
         circuit = Circuit(2)
         circuit.cnot(0, 1)
         pattern = transpile_brickwork(circuit).pattern
@@ -22,7 +25,9 @@ class TestBrickworkTranspilerUnitGates:
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
 
-    def test_rx(self, fx_rng: Generator) -> None:  # noqa: D102
+    @staticmethod
+    def test_rx(fx_rng: Generator) -> None:
+        """Test RX transpilation."""
         theta = fx_rng.uniform() * 2 * np.pi
         circuit = Circuit(2)
         circuit.rx(0, theta)
@@ -31,7 +36,9 @@ class TestBrickworkTranspilerUnitGates:
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
 
-    def test_rz(self, fx_rng: Generator) -> None:  # noqa: D102
+    @staticmethod
+    def test_rz(fx_rng: Generator) -> None:
+        """Test RZ transpilation."""
         theta = fx_rng.uniform() * 2 * np.pi
         circuit = Circuit(2)
         circuit.rz(0, theta)
@@ -40,7 +47,9 @@ class TestBrickworkTranspilerUnitGates:
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
 
-    def test_i(self, fx_rng: Generator) -> None:
+    @staticmethod
+    def test_i(fx_rng: Generator) -> None:
+        """Test I transpilation."""
         circuit = Circuit(2)
         circuit.i(0)
         pattern = transpile_brickwork(circuit).pattern
@@ -48,7 +57,9 @@ class TestBrickworkTranspilerUnitGates:
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
 
-    def test_empty_x_z(self, fx_rng: Generator) -> None:
+    @staticmethod
+    def test_empty_x_z(fx_rng: Generator) -> None:
+        """Test with empty X and Z gates."""
         circuit = Circuit(4)
         for i in [0, 1, 2]:
             for j in [0, 1, 2, 3, 4]:
@@ -59,7 +70,9 @@ class TestBrickworkTranspilerUnitGates:
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
 
-    def test_multi_gate(self, fx_rng: Generator) -> None:
+    @staticmethod
+    def test_multi_gate(fx_rng: Generator) -> None:
+        """Test with multiple gates."""
         circuit = Circuit(4)
         circuit.cnot(0, 1)
         circuit.rx(0, np.pi / 4)
@@ -70,7 +83,9 @@ class TestBrickworkTranspilerUnitGates:
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
 
-    def test_multi_gate_against_default(self, fx_rng: Generator) -> None:
+    @staticmethod
+    def test_multi_gate_against_default(fx_rng: Generator) -> None:
+        """Test with multiple gates and compare to current Graphix transpiler."""
         circuit = Circuit(4)
         circuit.cnot(0, 1)
         circuit.rx(0, np.pi / 4)
@@ -82,7 +97,9 @@ class TestBrickworkTranspilerUnitGates:
         state_mbqc_brickwork = pattern_brickwork.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc_default.flatten().conjugate(), state_mbqc_brickwork.flatten())) == pytest.approx(1)
 
-    def test_multi_gate_deep(self, fx_rng: Generator) -> None:
+    @staticmethod
+    def test_multi_gate_deep(fx_rng: Generator) -> None:
+        """Test with multiple gates in multiple layers."""
         circuit = Circuit(4)
         for j in [0, 1, 2, 3, 4]:
             circuit.rx(0, j * np.pi / 4)
@@ -92,7 +109,9 @@ class TestBrickworkTranspilerUnitGates:
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
 
-    def test_multi_gate_deviant(self, fx_rng: Generator) -> None:
+    @staticmethod
+    def test_multi_gate_deviant(fx_rng: Generator) -> None:
+        """Test with construction folling the deviant pattern."""
         circuit = Circuit(4)
         circuit.cnot(0, 1)
         circuit.rx(0, np.pi / 4)
@@ -103,7 +122,9 @@ class TestBrickworkTranspilerUnitGates:
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
 
-    def test_multi_gate_deviant_right(self, fx_rng: Generator) -> None:
+    @staticmethod
+    def test_multi_gate_deviant_right(fx_rng: Generator) -> None:
+        """Test with construction folling the deviant right pattern."""
         circuit = Circuit(4)
         circuit.cnot(0, 1)
         circuit.rx(0, np.pi / 4)
@@ -114,7 +135,9 @@ class TestBrickworkTranspilerUnitGates:
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
 
-    def test_single_qubit(self, fx_rng: Generator) -> None:
+    @staticmethod
+    def test_single_qubit(fx_rng: Generator) -> None:
+        """Test with single qubit gates."""
         n = 6
         circuit = Circuit(n)
         for i in range(n - 1):
@@ -125,23 +148,23 @@ class TestBrickworkTranspilerUnitGates:
         state_mbqc = pattern.simulate_pattern(rng=fx_rng)
         assert np.abs(np.dot(state_mbqc.flatten().conjugate(), state.flatten())) == pytest.approx(1)
 
-    def test_cnot_nonlocal(self) -> None:
+    @staticmethod
+    def test_cnot_nonlocal() -> None:
+        """Check error for nonlocal CNOTs."""
         circuit = Circuit(3)
         circuit.cnot(0, 2)
         with pytest.raises(ValueError):
-            transpile_brickwork(circuit).pattern
+            transpile_brickwork(circuit)
 
-    def test_other_gate(self) -> None:
-        circuit = Circuit(1)
-        circuit.h(0)
-        with pytest.raises(ValueError):
-            transpile_brickwork(circuit).pattern
-
-    def test_nqubits_from_layers_0(self) -> None:
+    @staticmethod
+    def test_nqubits_from_layers_0() -> None:
+        """Check error for empty layers."""
         with pytest.raises(ValueError):
             nqubits_from_layers([])
 
-    def test_nqubits_from_layers_n(self) -> None:
+    @staticmethod
+    def test_nqubits_from_layers_n() -> None:
+        """Check nqubits_from_layers function behaves as expected."""
         for i in range(2, 4):
             circuit = Circuit(i)
             for j in range(i):
@@ -149,7 +172,9 @@ class TestBrickworkTranspilerUnitGates:
             layers = transpile_to_layers(circuit)
             assert nqubits_from_layers(layers) == i
 
-    def test_parametrized_circuit(self, fx_rng: Generator) -> None:
+    @staticmethod
+    def test_parametrized_circuit(fx_rng: Generator) -> None:
+        """Test with parametrized circuit."""
         circuit = Circuit(4)
         circuit.cnot(0, 1)
         alpha = Placeholder("alpha")
